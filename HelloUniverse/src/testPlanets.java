7c978afd8bfda5e57b5ef53d1d40f6f7994a1f0e
Yoann <martinezyoann83@gmail.com>
2025-10-21 15:26:47 +0200

    base instructions

--- HelloUniverse/src/testPlanets.java
+++ HelloUniverse/src/testPlanets.java
@@ -1,20 +1,78 @@
 public class testPlanets {
 
     public static void main(String[] args) {
-        new testPlanets((short) 2006);
+        new testPlanets(); // constructor first methode
     }
 
-    private testPlanets(short yearTesting){
-        int nbPlanetes=8;
-        short year = 2003;
-        String phrase="En " + year + ", les planètes du système solaire étaient au nombre de : " + nbPlanetes;
+    private testPlanets(){
+        afficherPlanetesParAnnee((short) 2003);
+        afficherHistoriquePlanetes();
+    }
 
+    // Calcule le nombre de planètes selon l'années
+    private int calculerNbPlanetes(short year) {
+        return (year < 2006) ? 9 : 8;
+    }
 
-        if (year < yearTesting) {
-            phrase="En " + year + ", les planètes du système solaire étaient au nombre de : " + ++nbPlanetes;
-            System.out.println(phrase);
-        } else {
-            System.out.println(phrase);
+    // Construit le message
+    private String construireMessage(short year, int nbPlanetes) {
+        return "En " + year + ", les planètes du système solaire étaient au nombre de : " + nbPlanetes;
+    }
+
+    // Affiche les planètes pour une année donnée
+    private void afficherPlanetesParAnnee(short year){
+        int nbPlanetes = calculerNbPlanetes(year);
+        String phrase = construireMessage(year, nbPlanetes);
+        System.out.println(phrase);
+    }
+
+    // Affiche l'historique complet
+    private void afficherHistoriquePlanetes(){
+        for (int nbPlanetes = 7; nbPlanetes <= 9; nbPlanetes++) {
+            afficherMessageParNombre(nbPlanetes);
+        }
+    }
+
+    // Obtient le message selon le nombre de planètes
+    private void afficherMessageParNombre(int nbPlanetes) {
+        String message = obtenirMessageAvecSwitch(nbPlanetes);
+        System.out.println(message);
+
+        // obtenirMessageAvecWhile(nbPlanetes);
+    }
+
+    // Option avec Switch
+    private String obtenirMessageAvecSwitch(int nbPlanetes) {
+        switch (nbPlanetes) {
+            case 7:
+                return "On sait qu'au 16ème siècle, seules 7 planètes avaient été découvertes";
+            case 8:
+                return "On sait que le nombre de planètes est passé de 7 à 8 au 17ème siècle, mais il a également été réduit de 9 à 8 en 2006";
+            case 9:
+                return "On sait que le nombre de planètes est passé de 8 à 9 au 18ème siècle et ce jusqu'en 2006, où ce nombre a été réduit à 8";
+            default:
+                return "Le programme ne peut pas fournir de résultat pour " + nbPlanetes;
+        }
+    }
+
+    // Option avec While
+    private void obtenirMessageAvecWhile(int nbPlanetes) {
+        while (nbPlanetes < 10) {
+            switch(nbPlanetes) {
+                case 7:
+                    System.out.println("On sait qu'au 16ème siècle, seules 7 planètes avaient été découvertes");
+                    break;
+                case 8:
+                    System.out.println("On sait que le nombre de planètes est passé de 7 à 8 au 17ème siècle, mais il a également été réduit de 9 à 8 en 2006");
+                    break;
+                case 9:
+                    System.out.println("On sait que le nombre de planètes est passé de 8 à 9 au 18ème siècle et ce jusqu'en 2006, où ce nombre a été réduit à 8");
+                    break;
+                default:
+                    System.out.printf("Le programme ne peut pas fournir de résultat pour %d", nbPlanetes);
+                    break;
+            }
+            nbPlanetes++;
         }
     }
 }
